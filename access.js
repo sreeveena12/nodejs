0506d6bf26c96802cf4d789e4372f09c88fd0ad4
Sree Puli <Sree Puli>
2017-04-27 11:12:31 -0700

    access.js

---
+++ access.js
@@ -0,0 +1,9 @@
+	function access(i)
+	{
+		if(i ==="sree"){
+		console.log("Allow access");
+		}else if(i === "aakash"){
+		console.log("Do not allow access");
+		}else
+		console.log("I don't know you");
+	}
